db19d3a3151169fbb305aa88c84d37997c0b85ee
Shashank Venkat <shashyv@gmail.com>
2021-08-08 21:56:06 +0530

    Configured buttons

--- app/src/main/java/com/example/calculator_java/MainActivity.java
+++ app/src/main/java/com/example/calculator_java/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.calculator_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
+
+    private TextView text;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
+    private Button button_0;
+    private Button button_plus;
+    private Button button_minus;
+    private Button button_multiply;
+    private Button button_divide;
+    private Button button_decimal;
+    private Button button_clear;
+    private char operator;
+    private float result=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_1=findViewById(R.id.button_1);
+        button_2=findViewById(R.id.button_2);
+        button_3=findViewById(R.id.button_3);
+        button_4=findViewById(R.id.button_4);
+        button_5=findViewById(R.id.button_5);
+        button_6=findViewById(R.id.button_6);
+        button_7=findViewById(R.id.button_7);
+        button_8=findViewById(R.id.button_8);
+        button_9=findViewById(R.id.button_9);
+        button_0=findViewById(R.id.button_0);
+        button_decimal=findViewById(R.id.button_decimal);
+        button_clear=findViewById(R.id.button_clear);
+        button_plus=findViewById(R.id.button_plus);
+        button_minus=findViewById(R.id.button_minus);
+        button_divide=findViewById(R.id.button_divide);
+        button_multiply=findViewById(R.id.button_multiply);
+        button_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text.setText(" ");
+            }
+        }) ;
+        
     }
 }
