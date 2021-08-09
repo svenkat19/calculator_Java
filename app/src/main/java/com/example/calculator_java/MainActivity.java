118591b013cae49e373dbd03cdccb0441bc3b12b
Shashank Venkat <shashyv@gmail.com>
2021-08-09 11:55:52 +0530

    Numerical Funtionalities

--- app/src/main/java/com/example/calculator_java/MainActivity.java
+++ app/src/main/java/com/example/calculator_java/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculator_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,29 +13,31 @@ public class MainActivity extends AppCompatActivity
 {
 
 
-    private TextView text;
-    private Button button_1;
-    private Button button_2;
-    private Button button_3;
-    private Button button_4;
-    private Button button_5;
-    private Button button_6;
-    private Button button_7;
-    private Button button_8;
-    private Button button_9;
-    private Button button_0;
-    private Button button_plus;
-    private Button button_minus;
-    private Button button_multiply;
-    private Button button_divide;
-    private Button button_decimal;
-    private Button button_clear;
-    private char operator;
-    private float result=0;
+     TextView text;
+     char operator;
+     double num=0;
+     double result=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+         Button button_1;
+         Button button_2;
+         Button button_3;
+         Button button_4;
+         Button button_5;
+         Button button_6;
+         Button button_7;
+         Button button_8;
+         Button button_9;
+         Button button_0;
+         Button button_plus;
+         Button button_minus;
+         Button button_multiply;
+         Button button_divide;
+         Button button_decimal;
+         Button button_clear;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text);
         button_1=findViewById(R.id.button_1);
         button_2=findViewById(R.id.button_2);
         button_3=findViewById(R.id.button_3);
@@ -51,12 +54,88 @@ public class MainActivity extends AppCompatActivity
         button_minus=findViewById(R.id.button_minus);
         button_divide=findViewById(R.id.button_divide);
         button_multiply=findViewById(R.id.button_multiply);
+
         button_clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                text.setText(" ");
+                text.setText("0");
+                result=0;
+                num=0;
             }
         }) ;
-        
+        button_0.setOnClickListener(new View.OnClickListener() {
+                                        @SuppressLint("SetTextI18n")
+                                        @Override
+                                        public void onClick(View v) {
+                                            num = num * 10;
+                                            num+=0;
+                                            text.setText(text.getText()+String.valueOf(num));
+                                        }
+                                    });
+        button_1.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=1;
+                    String s=String.valueOf(num);
+                    text.setText(s);
+                }
+        );
+        button_2.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=2;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_3.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=3;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_4.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=4;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_5.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=5;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_6.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=6;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_7.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=7;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_8.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=8;
+                    text.setText(String.valueOf(num));
+                }
+        );
+        button_9.setOnClickListener((View v)->{
+
+                    num=num*10;
+                    num+=9;
+                    text.setText(String.valueOf(num));
+                }
+        );
+
     }
 }
